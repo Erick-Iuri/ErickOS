3bd4bcac8e8fa6f0c311a489833ad7e46fff3bd7
Erick <128732040+Erick-Iuri@users.noreply.github.com>
2026-06-11 16:03:57 -0300

    Scroll bar

--- components/janela.tsx
+++ components/janela.tsx
@@ -95,9 +95,9 @@ export default function Janela({ onFechar, isOpen }: JanelaProps) {
     >
       {/* Janela Sobre Mim ☕ */}
       <div
-        /* seleciona a janela que vai mover */
-        className="w-7xl h-220 absolute shadow-2xl
-
+        /* Tamanho da janela */
+        className="w-7xl h-220 absolute shadow-2xl overflow-hidden 
+          janela-scroll
           bg-[#1E1E1E] rounded-sm "
         style={{
           left: positionJanelaX,
@@ -298,87 +298,94 @@ export default function Janela({ onFechar, isOpen }: JanelaProps) {
           </div>
         </div>
 
-        {/* conteúdo */}
-        <div className="flex justify-center gap-7">
-          {/* coluna H1 1/2 */}
-          <div className="flex justify-start flex-col text-white pt-10">
-            <text className="text-2xl font-medium">
-              👋 Bem-vindo ao ErickOS
-            </text>
-            <text className="pt-7 w-3xl text-xl font-medium">
-              Sou desenvolvedor Front-End em formação, apaixonado por criar
-              experiências digitais intuitivas e funcionais.
-            </text>
-            <text className="pt-7 w-xl font-light">
-              Fique à vontade para abrir arquivos, explorar pastas e conhecer um
-              pouco mais sobre minha forma de pensar, projetar e construir
-              produtos digitais.
-            </text>
-            {/* botões */}
-            <div className="flex justify-start pt-10 gap-3">
-              {/* Botão Github */}
-              <Link
-                href={"https://github.com/Erick-Iuri"}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div
-                  className="flex items-center justify-center
+        {/* Corpo */}
+        <div className="flex flex-col h-full overflow-y-auto">
+          <div className="flex justify-center gap-7 ">
+            {/* coluna H1 1/2 */}
+            <div className="flex justify-start flex-col text-white pt-10">
+              <p className="text-2xl font-medium">
+                👋 Bem-vindo ao ErickOS
+              </p>
+              <p className="pt-7 w-3xl text-xl font-medium">
+                Sou desenvolvedor Front-End em formação, apaixonado por criar
+                experiências digitais intuitivas e funcionais.
+              </p>
+              <p className="pt-7 w-xl font-light">
+                Fique à vontade para abrir arquivos, explorar pastas e conhecer
+                um pouco mais sobre minha forma de pensar, projetar e construir
+                produtos digitais.
+              </p>
+              {/* botões */}
+              <div className="flex justify-start pt-10 gap-3">
+                {/* Botão Github */}
+                <Link
+                  href={"https://github.com/Erick-Iuri"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div
+                    className="flex items-center justify-center
                   transition-all duration-200 ease-in-out 
                   hover:-translate-y-0.5 hover:shadow-lg
                   cursor-pointer
                   gap-2 px-5 py-1.5 
                   w-40 h-10
                   rounded-sm bg-[#E63946] text-md"
+                  >
+                    <Image
+                      alt="monitor"
+                      src="/icones/github-svgrepo-com 2.png"
+                      width={"20"}
+                      height={"20"}
+                      className="w-4 h-4 cursor-pointer"
+                    />
+                    <button className="cursor-pointer">Github</button>
+                  </div>
+                </Link>
+                {/* Botão linkeding */}
+                <Link
+                  href={"https://www.linkedin.com/in/erick-aniello-49953b163/"}
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <Image
-                    alt="monitor"
-                    src="/icones/github-svgrepo-com 2.png"
-                    width={"20"}
-                    height={"20"}
-                    className="w-4 h-4 cursor-pointer"
-                  />
-                  <button className="cursor-pointer">Github</button>
-                </div>
-              </Link>
-              {/* Botão linkeding */}
-              <Link
-                href={"https://www.linkedin.com/in/erick-aniello-49953b163/"}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div
-                  className="flex items-center justify-center
+                  <div
+                    className="flex items-center justify-center
                   transition-all duration-200 ease-in-out 
                   hover:-translate-y-0.5 hover:shadow-lg
                   cursor-pointer
                   gap-2 px-5 py-1.5 
                   w-40 h-10
                   rounded-sm bg-white text-md"
-                >
-                  <Image
-                    alt="monitor"
-                    src="/icones/linkedinglogo.png"
-                    width={"20"}
-                    height={"20"}
-                    className="w-4 h-4 cursor-pointer"
-                  />
-                  <button className="cursor-pointer text-[#007AB9] font-bold">
-                    Linkedin
-                  </button>
-                </div>
-              </Link>
+                  >
+                    <Image
+                      alt="monitor"
+                      src="/icones/linkedinglogo.png"
+                      width={"20"}
+                      height={"20"}
+                      className="w-4 h-4 cursor-pointer"
+                    />
+                    <button className="cursor-pointer text-[#007AB9] font-bold">
+                      Linkedin
+                    </button>
+                  </div>
+                </Link>
+              </div>
+            </div>
+            {/* Coluna H1 2/2 */}
+            <div className="flex justify-center">
+              <Image
+                alt="monitor"
+                src="/icones/banner café.png"
+                width={"600"}
+                height={"600"}
+                className="w-70 h-auto cursor-pointer p-5"
+              />
             </div>
           </div>
-          {/* Coluna H1 2/2 */}
-          <div className="flex justify-center">
-            <Image
-              alt="monitor"
-              src="/icones/banner café.png"
-              width={"600"}
-              height={"600"}
-              className="w-70 h-auto cursor-pointer p-5"
-            />
+
+          {/* Habilidades 🔥*/}
+          <div className="flex justify-center p-10">
+            <div className="w-full h-200 bg-red-800 "></div>
           </div>
         </div>
       </div>
